f2e3a94ad8513444231185d6a780c76b3b834929
David Simek <david.simek1998@gmail.com>
2021-07-18 19:34:29 +0200

    added sellTokens feature + tests

--- src/contracts/UndercoverSwap.sol
+++ src/contracts/UndercoverSwap.sol
@@ -7,6 +7,20 @@ contract UndercoverSwap {
   Token public token;
   uint public rate = 10;
 
+  event TokenPurchased(
+    address account,
+    address token,
+    uint amount,
+    uint rate
+  );
+
+  event TokenSold(
+      address account,
+      address token,
+      uint amount,
+      uint rate
+    );
+
   constructor(Token _token) public {
     token = _token;
   }
@@ -16,7 +30,25 @@ contract UndercoverSwap {
     uint tokenAmount = msg.value * rate;
     // because UCT use only 9 decimal places we have to divide our rate
     tokenAmount = tokenAmount / (10**(18-9));
+
+    require(token.balanceOf(address(this)) >= tokenAmount);
+
     token.transfer(msg.sender, tokenAmount);
+    // Emit on event
+    emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
   }
 
+  function sellTokens(uint _amount) public {
+    require(token.balanceOf(msg.sender) >= _amount);
+
+    uint ethAmount = _amount / rate;
+    ethAmount = ethAmount * (10**(18-9));
+
+    require(address(this).balance >= ethAmount);
+
+    token.transferFrom(msg.sender, address(this), _amount);
+    msg.sender.transfer(ethAmount);
+
+    emit TokenSold(msg.sender, address(token), _amount, rate);
+  }
 }
--- test/UndercoverSwap.test.js
+++ test/UndercoverSwap.test.js
@@ -39,12 +39,65 @@ contract('UndercoverSwap', (accounts) => {
   })
 
   describe('UndercoverSwap possibility to buy tokens', async() => {
+    let result;
+
+    before(async() => {
+      // Purchase tokens before every test
+      result = await undercoverSwap.buyTokens({
+         from: accounts[0],
+         value: web3.utils.toWei('1', 'ether')
+       })
+    })
+
     it('Allow users to buy UCT for fixed price', async() => {
-      await undercoverSwap.buyTokens(
-        {from: accounts[0], value: web3.utils.toWei('1', 'ether')}
+      let investor = accounts[0];
+      let investorBalance = await token.balanceOf(investor)
+      assert.equal(investorBalance.toString(), tokens('10'))
+
+      let undercoverSwapBalanace = await token.balanceOf(undercoverSwap.address)
+      assert.equal(undercoverSwapBalanace.toString(), tokens('9990'))
+      let ethUndercoverSwatpBalance = await web3.eth.getBalance(undercoverSwap.address)
+      assert.equal(ethUndercoverSwatpBalance.toString(), web3.utils.toWei('1', 'ether'))
+
+      let event = result.logs[0].args
+      assert.equal(event.account, investor)
+      assert.equal(event.token, token.address)
+      assert.equal(event.amount, tokens(10))
+      assert.equal(event.rate, '10')
+    })
+  })
+
+  describe('UndercoverSwap possibility to sell tokens', async() => {
+    let result
+
+    before(async() => {
+      await token.approve(undercoverSwap.address, tokens('10'), {from: accounts[0]})
+      result = await undercoverSwap.sellTokens(
+        tokens('10'), {from: accounts[0]}
       )
-      let balance = await token.balanceOf(accounts[0])
-      assert.equal(balance.toString(), tokens('10'))
+    })
+
+    it('Allows users to sell their tokens for eth', async() => {
+      let seller = accounts[0];
+      let sellerBalance = await token.balanceOf(seller)
+      assert.equal(sellerBalance.toString(), tokens('0'))
+
+      let undercoverSwapBalance = await web3.eth.getBalance(undercoverSwap.address)
+      assert.equal(undercoverSwapBalance.toString(), web3.utils.toWei('0'))
+
+      let uctUndercoverSwapBalance = await token.balanceOf(undercoverSwap.address)
+      assert.equal(uctUndercoverSwapBalance.toString(), tokens('10000'))
+
+      let event = result.logs[0].args
+      assert.equal(event.account, accounts[0])
+      assert.equal(event.token, token.address)
+      assert.equal(event.amount, tokens(10))
+      assert.equal(event.rate, '10')
+
+      await undercoverSwap.sellTokens(
+        tokens('100'),
+         {from: accounts[0]}
+       ).should.be.rejected;
     })
   })
 
